ce7c4a5804fce5d165ca2de41789023af440eeb2
Tshiamo Lephale <tslephal@c4r9s1.wethinkcode.co.za>
2020-07-09 12:05:27 +0200

    first version of the .c file

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void	ft_print_alphabet(void)
+{
+	int count,char_reference;
+	count = 0;
+	char_reference = 97;
+
+	while (count < 26)
+	{
+		ft_putchar(char_reference + count);
+		count += 1;
+	}
+	ft_putchar('\n');
+	
+}
+
+int		main(void)
+{
+	ft_print_alphabet();
+	return(0);
+}
