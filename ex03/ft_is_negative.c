e15b8a698919b6b845c32c9cd2306a34060ed119
Tshiamo Lephale <tslephal@c4r9s1.wethinkcode.co.za>
2020-07-09 16:11:49 +0200

    added the file for displaying whether integer is negative or not

---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,15 @@
+int ft_putchar(char c);
+
+void ft_is_negative(int integer)
+{
+	if (integer >= 0)
+	{
+		ft_putchar(80);
+		ft_putchar('\n');
+	}
+	else
+	{
+		ft_putchar(78);
+		ft_putchar('\n');
+	}
+}
